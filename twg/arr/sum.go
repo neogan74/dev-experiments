29cd5e5f211a82e1909850f26a7fa1b705171f50
Andrei Gushchin <neogan74@gmail.com>
2024-12-28 21:25:16 +0300

    tdd sums

--- twg/arr/sum.go
+++ twg/arr/sum.go
@@ -15,3 +15,16 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(numToTailSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numToTailSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- twg/arr/sum_test.go
+++ twg/arr/sum_test.go
@@ -38,3 +38,23 @@ func TestAllSumm(t *testing.T) {
 		t.Errorf("got %v want %v given %v and %v", got, want, s1, s2)
 	}
 }
+
+func TestSullAllTail(t *testing.T) {
+	t.Run("make sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
