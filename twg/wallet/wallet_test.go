2191e54355992b1050acaaa09fb6ed469b46eaa4
Andrei Gushchin <neogan74@gmail.com>
2024-12-29 17:25:43 +0300

    tdd wall ref

--- twg/wallet/wallet_test.go
+++ twg/wallet/wallet_test.go
@@ -4,28 +4,29 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-		got := wallet.Balance()
-		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
 	})
 
 }
