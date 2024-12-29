bfba1fce067871b9f849bae0203d72045198da01
Andrei Gushchin <neogan74@gmail.com>
2024-12-29 09:37:56 +0300

    tdd geo ref

--- twg/geometry/geo.go
+++ twg/geometry/geo.go
@@ -1,9 +1,14 @@
 package geometry
 
-func Perimeter(w, h float64) float64 {
-	return 2 * (w + h)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(w, h float64) float64 {
-	return w * h
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
