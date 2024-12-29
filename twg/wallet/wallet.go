9c1dde95d972da8fae8ed144054c6967af74d043
Andrei Gushchin <neogan74@gmail.com>
2024-12-29 17:22:49 +0300

    tdd wall ref

--- twg/wallet/wallet.go
+++ twg/wallet/wallet.go
@@ -1,13 +1,29 @@
 package wallet
 
-type Wallet struct {
-	balance int
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
