7449434dc667ac76efb8e59f8f152167dcc5935f
Andrei Gushchin <neogan74@gmail.com>
2024-12-29 10:04:14 +0300

    tdd geo ref

--- twg/geometry/geo_test.go
+++ twg/geometry/geo_test.go
@@ -17,6 +17,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestAria(t *testing.T) {
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
+	}
+
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
