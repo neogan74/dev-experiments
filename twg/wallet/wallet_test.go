90669abde22091ac467b6d783bcefc0cd51fe2e2
Andrei Gushchin <neogan74@gmail.com>
2024-12-29 19:49:11 +0300

    tdd wall ref

--- twg/wallet/wallet_test.go
+++ twg/wallet/wallet_test.go
@@ -13,6 +13,17 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -33,12 +44,9 @@ func TestWallet(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
-
+		assertError(t, err, "cannot withdraw, insufficent funds")
 		assertBalance(t, wallet, startingBalance)
 
-		if err == nil {
-			t.Errorf("wanted an error but didn't get one")
-		}
 	})
 
 }
