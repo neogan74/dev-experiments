891a602e4480a6b68a240f381cb4bf67f0b288a4
Andrei Gushchin <neogan74@gmail.com>
2025-01-01 19:57:49 +0300

    tdd mock ref

--- twg/mock/countdown.go
+++ twg/mock/countdown.go
@@ -10,14 +10,33 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func main() {
-	Countdown(os.Stdout)
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
 
-func Countdown(out io.Writer) {
+func main() {
+	sleeper := DefaultSleeper{}
+	Countdown(os.Stdout, &sleeper)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(time.Second * 1)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
