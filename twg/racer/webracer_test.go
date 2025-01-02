9f725944543760eb216a7c99f4ac234a4d86827c
Andrei Gushchin <neogan74@gmail.com>
2025-01-02 22:09:42 +0300

    tdd racer init

--- twg/racer/webracer_test.go
+++ twg/racer/webracer_test.go
@@ -8,20 +8,39 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDeleayedServer(20 * time.Millisecond)
-	fastServer := makeDeleayedServer(0 * time.Millisecond)
+	t.Run("compares speeds of servers, returning the url of the fastest one ", func(t *testing.T) {
+		slowServer := makeDeleayedServer(20 * time.Millisecond)
+		fastServer := makeDeleayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+		if err != nil {
+			t.Fatal("An error happens")
+		}
+	})
+
+	t.Run("returns an error if a server doesnt respond withing 10s", func(t *testing.T) {
+		serverA := makeDeleayedServer(11 * time.Second)
+		serverB := makeDeleayedServer(21 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error after 10 sec but didn't get one")
+		}
+	})
 
 }
 
