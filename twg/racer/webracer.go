d5ec909ff6ef9172f39fe194bd524ff204e15b33
Andrei Gushchin <neogan74@gmail.com>
2025-01-02 21:50:09 +0300

    tdd racer init

--- twg/racer/webracer.go
+++ twg/racer/webracer.go
@@ -1,5 +1,22 @@
 package racer
 
-func Racer(firstURL, secondURL string) string {
-	return secondURL
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
