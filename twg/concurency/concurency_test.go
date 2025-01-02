95d8f24aabb99a40a75adde1b9869ebecd17def8
Andrei Gushchin <neogan74@gmail.com>
2025-01-02 17:24:10 +0300

    tdd concur init

--- twg/concurency/concurency_test.go
+++ twg/concurency/concurency_test.go
@@ -3,10 +3,16 @@ package concurency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
 	return url != "waat://furhurterwe.geds"
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
 }
 
 func TestCheckWebsites(t *testing.T) {
@@ -25,5 +31,16 @@ func TestCheckWebsites(t *testing.T) {
 	got := CheckWebsite(mockWebsiteChecker, websites)
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("wanted %v got %v", want, got)
+	}
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsite(slowStubWebsiteChecker, urls)
 	}
 }
