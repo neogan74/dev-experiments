548a20d4d0ef2138627af37e74eb5e4fc17dd7cc
Andrei Gushchin <neogan74@gmail.com>
2025-01-02 21:54:00 +0300

    tdd racer init

--- twg/racer/webracer_test.go
+++ twg/racer/webracer_test.go
@@ -1,15 +1,32 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	// slowURL := "http://www.facebook.com"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	// fastURL := "http://www.quii.dev"
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
 
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	defer slowServer.Close()
+	defer fastServer.Close()
 }
