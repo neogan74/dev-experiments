ab0f1a21f71fe05eb30ea34ed384ae7200968145
Andrei Gushchin <neogan74@gmail.com>
2025-01-03 20:50:32 +0300

    tdd reflection evolution

--- twg/reflection/walk.go
+++ twg/reflection/walk.go
@@ -1,5 +1,9 @@
 package reflection
 
-func walk(interface{}, func(intput string)) {
+import "reflect"
 
+func walk(x interface{}, fn func(intput string)) {
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
--- twg/reflection/walk_test.go
+++ twg/reflection/walk_test.go
@@ -14,6 +14,10 @@ func TestWalk(t *testing.T) {
 		got = append(got, input)
 	})
 
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
 	}
