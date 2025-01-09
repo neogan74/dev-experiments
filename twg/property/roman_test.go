37326bc15c643a0baf1250c9c81d4087d4577566
Andrei Gushchin <neogan74@gmail.com>
2025-01-09 18:03:01 +0300

    tdd rome evl

--- twg/property/roman_test.go
+++ twg/property/roman_test.go
@@ -10,6 +10,7 @@ func TestRomanNumerals(t *testing.T) {
 	}{
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to I", 2, "II"},
+		{"3 gets converted to I", 3, "III"},
 	}
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
