bb3d555b63e886843f2841ddaaeda9ffe9f77927
Andrei Gushchin <neogan74@gmail.com>
2025-01-09 18:37:32 +0300

    tdd rome evl

--- twg/property/roman.go
+++ twg/property/roman.go
@@ -2,27 +2,29 @@ package property
 
 import "strings"
 
+type Roman struct {
+	Value  int
+	Symbol string
+}
+
+var allRomansNumerals = []Roman{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, numeral := range allRomansNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
-
 	return result.String()
 
 }
