f11ca149342ebadb1040ad9d180d9a3df85e6d07
Andrei Gushchin <neogan74@gmail.com>
2025-01-12 12:54:30 +0300

    tdd clock init

--- twg/clockface/clock_test.go
+++ twg/clockface/clock_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, time.UTC)
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := Point{X: 150, Y: 150 - 90}
+	got := SecondHand(tm)
 
 	if got != want {
 		t.Errorf("Got %v, want %v", got, want)
