bbec4277b0f17324b235b80b380d5a9c5b8090d9
Andrei Gushchin <neogan74@gmail.com>
2025-01-13 16:25:42 +0300

    tdd clock evl

--- twg/clockface/clock_test.go
+++ twg/clockface/clock_test.go
@@ -15,3 +15,14 @@ func TestSecondHandAtMidnight(t *testing.T) {
 		t.Errorf("Got %v, want %v", got, want)
 	}
 }
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, want %v", got, want)
+	}
+}
