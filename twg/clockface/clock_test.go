151d426920c70fc40bd742614cf539ebb4ec891d
Andrei Gushchin <neogan74@gmail.com>
2025-01-17 20:49:53 +0300

    tdd clock evl

--- twg/clockface/clock_test.go
+++ twg/clockface/clock_test.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"strings"
 	"testing"
 	"time"
 )
+
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
+}
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
@@ -70,8 +97,20 @@ func TestSecondHandPoint(t *testing.T) {
 func TestSVGWritterAtMidnight(t *testing) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	var b strings.Builder
+	var b bytes.Buffer{}
 	SVGWriter(&b, tm)
+
+	svg := SVG{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 = "150"
+	y2 = "60"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
 	got := b.String()
 
 	want := `<line x1="150" y1="150" x2="150" y2="60"`
