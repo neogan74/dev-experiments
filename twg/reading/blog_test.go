2d8938ae0ac10db3be678a443b39cd8763360f62
Andrei Gushchin <neogan74@gmail.com>
2025-01-27 18:39:04 +0300

    tdd rading evl

--- twg/reading/blog_test.go
+++ twg/reading/blog_test.go
@@ -1,23 +1,20 @@
 package reading
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
+	got := posts[0]
+	want := Post{Title: "Post 1"}
 
-	posts, err := NewPostsFromFS(fs)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(posts) != len(fs) {
+	if !reflect.DeepEqual(got , want)
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
 }
