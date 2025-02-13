4a12642cfac1d5d2c6c166ece1c3926fbd1c7fa5
Andrei Gushchin <neogan74@gmail.com>
2025-02-13 20:50:32 +0300

    tdd srv evl

--- twg/srv/main.go
+++ twg/srv/main.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+// InMemoryPlayerStore collects data about players in memory.
+type InMemoryPlayerStore struct{}
+
+// GetPlayerScore retrieves scores for a given player.
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
+	handler := &PlayerServer(&InMemoryPlayerStore{})
 	log.Fatal(http.ListenAndServe(":5000", handler))
 }
