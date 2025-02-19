34d0fd8d0dc5662f8f0265f7e1eb1f32e76f824a
Andrei Gushchin <neogan74@gmail.com>
2025-02-19 18:37:39 +0300

    one

--- hangman/cmd/hangman/main.go
+++ hangman/cmd/hangman/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("Добро пожаловать! это Игра Hangman")
+	fmt.Println("Правила: ")
+	fmt.Println("Пробуем угадать слово по вопросу ")
 }
