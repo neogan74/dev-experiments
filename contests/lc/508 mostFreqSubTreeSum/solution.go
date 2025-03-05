34cb81203df7bf283b382d331572e77aebcb6327
Andrei Gushchin <neogan74@gmail.com>
2025-03-05 09:51:23 +0300

    lc 508

--- contests/lc/508 mostFreqSubTreeSum/solution.go
+++ contests/lc/508 mostFreqSubTreeSum/solution.go
@@ -4,39 +4,36 @@ func main() {
 }
 
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
-
-
 
 func findFrequentTreeSum(root *TreeNode) []int {
-    sumFreq := make(map[int]int)
-    maxFreq := 0
+	sumFreq := make(map[int]int)
+	maxFreq := 0
 
-    var subtreeSum func(node *TreeNode) int
-    subtreeSum = func(node *TreeNode) int {
-        if node == nil {
-            return 0
-        }
-        leftSum := subtreeSum(node.Left)
-        rightSum := subtreeSum(node.Right)
-        currSum := node.Val + leftSum + rightSum
-        sumFreq[currSum]++
-        if sumFreq[currSum] > maxFreq {
-            maxFreq = sumFreq[currSum]
-        }
-        return currSum
-    }
+	var subtreeSum func(node *TreeNode) int
+	subtreeSum = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		leftSum := subtreeSum(node.Left)
+		rightSum := subtreeSum(node.Right)
+		currSum := node.Val + leftSum + rightSum
+		sumFreq[currSum]++
+		if sumFreq[currSum] > maxFreq {
+			maxFreq = sumFreq[currSum]
+		}
+		return currSum
+	}
 
-    subtreeSum(root)
-    var result []int
-    for sum, freq := range sumFreq {
-        if freq == maxFreq {
-            result = append(result, sum)
-        }
-    }
-    return result
-}
+	subtreeSum(root)
+	var result []int
+	for sum, freq := range sumFreq {
+		if freq == maxFreq {
+			result = append(result, sum)
+		}
+	}
+	return result
 }
