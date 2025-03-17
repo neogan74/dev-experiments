bff67dba3bb8843f623a69ed23cc1283836b8e63
Andrei Gushchin <neogan74@gmail.com>
2025-03-17 09:45:15 +0300

    lc 274 solution

---
+++ contests/lc/274.H-Index/solution.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func hIndex(citations []int) int {
+	sort.Ints(citations)
+	n := len(citations)
+	for i, _ := range citations {
+		h := n - i // Number of papers with at least `citations[i]` citations
+		if citations[i] >= h {
+			return h // Maximum valid `h` found
+		}
+	}
+	return 0
+}
+
+func main() {
+	citations := []int{3, 0, 6, 1, 5}
+	res := hIndex(citations)
+	fmt.Printf("Result %v\n", res)
+	citations2 := []int{1, 3, 1}
+	res = hIndex(citations2)
+	fmt.Printf("result %v\n", res)
+
+}
