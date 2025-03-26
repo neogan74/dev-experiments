46224b2878885e244ead60089bca5ea33b8b309a
Andrei Gushchin <neogan74@gmail.com>
2025-03-26 20:23:19 +0300

    lc 2033 solution

---
+++ contests/lc/2033. Minimum Operations to Make a Uni-Value Grid/solution.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func minOperations(grid [][]int, x int) int {
+	values := []int{}
+
+	for _, row := range grid {
+		for _, val := range row {
+			values = append(values, val)
+		}
+	}
+
+	sort.Ints(values)
+
+	for _, val := range values {
+		if int(math.Abs(float64(val-values[0])))%x != 0 {
+			return -1
+		}
+	}
+
+	median := values[len(values)/2]
+	operations := 0
+
+	for _, val := range values {
+		operations += int(math.Abs(float64(val-median))) / x
+	}
+
+	return operations
+}
+
+func main() {
+	grid := [][]int{{2, 4}, {6, 8}}
+	x := 2
+	res := minOperations(grid, x)
+	fmt.Println(res)
+}
