3038e223759db1310397913c6ca2821c44f07bcd
Andrei Gushchin <neogan74@gmail.com>
2025-03-27 22:16:46 +0300

    lc 2780 test

---
+++ contests/lc/2780. Minimum Index of a Valid Split/solution_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_minimumIndex(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "First case",
+			args: args{
+				nums: []int{1, 2, 2, 2},
+			},
+			want: 2,
+		},
+		{name: "Second case",
+			args: args{
+				nums: []int{2, 1, 3, 1, 1, 1, 7, 1, 2, 1},
+			},
+			want: 4,
+		},
+		{name: "Third case",
+			args: args{
+				nums: []int{3, 3, 3, 3, 7, 2, 2},
+			},
+			want: -1,
+		},
+	}
+	// TODO: Add test cases.
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumIndex(tt.args.nums); got != tt.want {
+				t.Errorf("minimumIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
