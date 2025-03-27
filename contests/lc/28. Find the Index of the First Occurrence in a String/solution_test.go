fda94075d8514895b8a0f0e3450593f621641df2
Andrei Gushchin <neogan74@gmail.com>
2025-03-27 22:23:47 +0300

    lc 28 test

---
+++ contests/lc/28. Find the Index of the First Occurrence in a String/solution_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "First",
+			args: args{
+				haystack: "sadbutsad",
+				needle:   "sad",
+			},
+			want: 0,
+		},
+		{name: "Second",
+			args: args{
+				haystack: "leetcode",
+				needle:   "leeto",
+			},
+			want: -1,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
