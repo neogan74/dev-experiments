38807f2d63ac84883c539e532a8453dd1aff5b4a
Andrei Gushchin <neogan74@gmail.com>
2025-04-05 10:02:41 +0300

    lc 68 solution

---
+++ contests/lc/68. Text Justification/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import "strings"
+
+func fullJustify(words []string, maxWidth int) []string {
+	var res []string
+	var cur []string
+	num_of_letters := 0
+
+	for _, word := range words {
+		if len(word)+len(cur)+num_of_letters > maxWidth {
+			for i := 0; i < maxWidth-num_of_letters; i++ {
+				cur[i%max(1, len(cur)-1)] += " "
+			}
+			res = append(res, strings.Join(cur, ""))
+			cur = cur[:0]
+			num_of_letters = 0
+		}
+		cur = append(cur, word)
+		num_of_letters += len(word)
+	}
+
+	lastLine := strings.Join(cur, " ")
+	for len(lastLine) < maxWidth {
+		lastLine += " "
+	}
+	res = append(res, lastLine)
+
+	return res
+}
+
+// Helper function to get the maximum of two integers
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
