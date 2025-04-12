0cab50a3fcf1f241074e8f43b8d82c16bee08584
Andrei Gushchin <neogan74@gmail.com>
2025-04-12 08:22:29 +0300

    chore: lc 383 test

---
+++ contests/lc/383.RansomNote/sol_test.go
@@ -0,0 +1,37 @@
+package _83_RansomNote
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOne(t *testing.T) {
+	got := one()
+	want := "coll"
+	if got != want {
+		fmt.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestCanConstruct(t *testing.T) {
+	tests := []struct {
+		ransomNote string
+		magazine   string
+		expected   bool
+	}{
+		{"a", "b", false},
+		{"aa", "ab", false},
+		{"aa", "aab", true},
+		{"", "abc", true},
+		{"abc", "", false},
+		{"abc", "aabbcc", true},
+	}
+
+	for _, tt := range tests {
+		result := canConstruct(tt.ransomNote, tt.magazine)
+		if result != tt.expected {
+			t.Errorf("canConstruct(%q, %q) = %v; want %v",
+				tt.ransomNote, tt.magazine, result, tt.expected)
+		}
+	}
+}
