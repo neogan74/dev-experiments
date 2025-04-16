42ac05fc830335a7854a458ca19d377c30730b40
Andrei Gushchin <neogan74@gmail.com>
2025-04-16 16:21:58 +0300

    chore: lc 242 sol

---
+++ contests/lc/242.ValidAnagram/valid_anagram.go
@@ -0,0 +1,37 @@
+package main
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	count := [26]int{}
+	for i := 0; i < len(s); i++ {
+		count[s[i]-'a']++
+		count[t[i]-'a']--
+	}
+
+	for _, c := range count {
+		if c != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func isAnagramUnicode(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	count := map[rune]int{}
+	for _, ch := range s {
+		count[ch]++
+	}
+	for _, ch := range t {
+		count[ch]--
+		if count[ch] < 0 {
+			return false
+		}
+	}
+	return true
+}
