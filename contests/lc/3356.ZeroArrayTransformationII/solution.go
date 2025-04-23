1296b0db5cff370b8f10aa6bf4ff89b96f47a271
Andrei Gushchin <neogan74@gmail.com>
2025-04-23 11:01:09 +0300

    fix: lc 3356 usage lint

--- contests/lc/3356.ZeroArrayTransformationII/solution.go
+++ contests/lc/3356.ZeroArrayTransformationII/solution.go
@@ -62,6 +62,35 @@ func zeroArrayTransformation(nums []int, queries [][]int) int {
 	return answer
 }
 
+// Greedily increment k while going through each number
+
+func minZeroArray2(nums []int, queries [][]int) int {
+	increments := make([]int, len(nums)+1)
+	k := 0
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		for sum+increments[i] < nums[i] {
+			if k >= len(queries) { // running out of queries
+				return -1
+			}
+			query := queries[k]
+			l := query[0]
+			r := query[1]
+			val := query[2]
+			// if r < i, the query doesn't improve increments[i], so just keep going to the next query
+			if r >= i {
+				// if l < i, we mark increments[i] so that it's effective immediately
+				increments[max(l, i)] += val
+				// subtract it down when getting outside of the range
+				increments[r+1] -= val
+			}
+			k++
+		}
+		sum += increments[i]
+	}
+	return k
+}
+
 func main() {
 	// Example 1:
 	nums1 := []int{2, 0, 2}
@@ -77,4 +106,20 @@ func main() {
 	nums3 := []int{0}
 	queries3 := [][]int{{0, 0, 2}, {0, 0, 4}, {0, 0, 4}, {0, 0, 3}, {0, 0, 5}}
 	fmt.Println(zeroArrayTransformation(nums3, queries3)) // Expected output: 0
+
+	// Example 1:
+	nums4 := []int{2, 0, 2}
+	queries4 := [][]int{{0, 2, 1}, {0, 2, 1}, {1, 1, 3}}
+	fmt.Println(minZeroArray2(nums4, queries4)) // Expected output: 2
+
+	// Example 2:
+	nums5 := []int{4, 3, 2, 1}
+	queries5 := [][]int{{1, 3, 2}, {0, 2, 1}}
+	fmt.Println(minZeroArray2(nums5, queries5)) // Expected output: -1
+
+	// Additional test case:
+	nums6 := []int{0}
+	queries6 := [][]int{{0, 0, 2}, {0, 0, 4}, {0, 0, 4}, {0, 0, 3}, {0, 0, 5}}
+	fmt.Println(minZeroArray2(nums6, queries6)) // Expected output: 0
+
 }
