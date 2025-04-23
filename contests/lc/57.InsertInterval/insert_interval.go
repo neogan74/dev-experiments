9e43c99dc8dfcea08683bf767e4b69d7d13c8dc1
Andrei Gushchin <neogan74@gmail.com>
2025-04-23 10:31:09 +0300

    chore: lc 57 solution

---
+++ contests/lc/57.InsertInterval/insert_interval.go
@@ -0,0 +1,20 @@
+package _7_InsertInterval
+
+import "sort"
+
+func insert(intervals [][]int, newInterval []int) [][]int {
+	merge := func(intervals [][]int) (ans [][]int) {
+		sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
+		ans = append(ans, intervals[0])
+		for _, e := range intervals[1:] {
+			if ans[len(ans)-1][1] < e[0] {
+				ans = append(ans, e)
+			} else {
+				ans[len(ans)-1][1] = max(ans[len(ans)-1][1], e[1])
+			}
+		}
+		return
+	}
+	intervals = append(intervals, newInterval)
+	return merge(intervals)
+}
