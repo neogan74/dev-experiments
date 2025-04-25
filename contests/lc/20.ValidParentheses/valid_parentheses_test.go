a4296c5beb90af26ebf46993afb34c54a5f814b6
Andrei Gushchin <neogan74@gmail.com>
2025-04-25 11:02:48 +0300

    chore: lc 20 test

---
+++ contests/lc/20.ValidParentheses/valid_parentheses_test.go
@@ -0,0 +1,37 @@
+package _0_ValidParentheses
+
+import "testing"
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Example 1",
+			args: args{
+				s: "()",
+			},
+			want: true,
+		},
+		{
+			name: "Example 2",
+			args: args{
+				s: "()[]{}",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
