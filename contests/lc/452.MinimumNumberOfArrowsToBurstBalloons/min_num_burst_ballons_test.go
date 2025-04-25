effd0ca3211cd0b1e5d37cd7fefdf7805c12d261
Andrei Gushchin <neogan74@gmail.com>
2025-04-25 11:04:24 +0300

    chore: lc 452 test

---
+++ contests/lc/452.MinimumNumberOfArrowsToBurstBalloons/min_num_burst_ballons_test.go
@@ -0,0 +1,47 @@
+package _52_MinimumNumberOfArrowsToBurstBalloons
+
+import "testing"
+
+func Test_findMinArrowShots(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantAns int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Example 1",
+			args: args{
+				points: [][]int{
+					{10, 16},
+					{2, 8},
+					{1, 6},
+					{7, 12},
+				},
+			},
+			wantAns: 2,
+		},
+		{
+			name: "Example 2",
+			args: args{
+				points: [][]int{
+					{1, 2},
+					{3, 4},
+					{5, 6},
+					{7, 8},
+				},
+			},
+			wantAns: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAns := findMinArrowShots(tt.args.points); gotAns != tt.wantAns {
+				t.Errorf("findMinArrowShots() = %v, want %v", gotAns, tt.wantAns)
+			}
+		})
+	}
+}
