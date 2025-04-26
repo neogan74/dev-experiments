d81a4303c8b779e0d91977994a2bd2b5a6be80a9
Andrei Gushchin <neogan74@gmail.com>
2025-04-26 04:22:38 +0300

    chore: lc 2799 solution

---
+++ contests/lc/2799.CountCompleteSubarraysInAnArray/count_complete_subarr.go
@@ -0,0 +1,19 @@
+package _799_CountCompleteSubarraysInAnArray
+
+func countCompleteSubarrays(nums []int) (ans int) {
+	s := map[int]bool{}
+	for _, x := range nums {
+		s[x] = true
+	}
+	cnt := len(s)
+	for i := range nums {
+		s = map[int]bool{}
+		for _, x := range nums[i:] {
+			s[x] = true
+			if len(s) == cnt {
+				ans++
+			}
+		}
+	}
+	return
+}
