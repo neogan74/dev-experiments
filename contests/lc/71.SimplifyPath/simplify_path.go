84087b58d0b79507ede25f247d7c9cf5faa3983d
Andrei Gushchin <neogan74@gmail.com>
2025-04-27 07:14:32 +0300

    chore: lc 71 solution

---
+++ contests/lc/71.SimplifyPath/simplify_path.go
@@ -0,0 +1,20 @@
+package _71_SimplifyPath
+
+import "strings"
+
+func simplifyPath(path string) string {
+	var stk []string
+	for _, s := range strings.Split(path, "/") {
+		if s == "" || s == "." {
+			continue
+		}
+		if s == ".." {
+			if len(stk) > 0 {
+				stk = stk[0 : len(stk)-1]
+			}
+		} else {
+			stk = append(stk, s)
+		}
+	}
+	return "/" + strings.Join(stk, "/")
+}
