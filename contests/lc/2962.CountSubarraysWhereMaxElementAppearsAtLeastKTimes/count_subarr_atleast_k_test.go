322fc2d1cebab04b7b83e989074e995cdf70142a
Andrei Gushchin <neogan74@gmail.com>
2025-04-29 10:12:47 +0300

    chore: lc 2962 test

---
+++ contests/lc/2962.CountSubarraysWhereMaxElementAppearsAtLeastKTimes/count_subarr_atleast_k_test.go
@@ -0,0 +1,39 @@
+package _962_CountSubarraysWhereMaxElementAppearsAtLeastKTimes
+
+import "testing"
+
+func Test_countSubarrays(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				nums: []int{1, 3, 2, 3, 3},
+				k:    2,
+			},
+			want: 6,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				nums: []int{1, 4, 2, 1},
+				k:    3,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSubarrays(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("countSubarrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
