8fc3316b9428bc547f309a8bcce38c18b88f097d
Andrei Gushchin <neogan74@gmail.com>
2025-04-30 10:19:32 +0300

    feat: lc 1295 solution

---
+++ contests/lc/1295.FindNumbersWithEvenNumberOfDigits/find_num_with_even_num_dig.go
@@ -0,0 +1,12 @@
+package __1295_FindNumbersWithEvenNumberOfDigits
+
+import "strconv"
+
+func findNumbers(nums []int) (ans int) {
+	for _, x := range nums {
+		if len(strconv.Itoa(x))%2 == 0 {
+			ans++
+		}
+	}
+	return
+}
