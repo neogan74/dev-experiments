574704bdfca0ac1b074a2a4f3f4fe17ec524edaf
Andrei Gushchin <neogan74@gmail.com>
2025-05-07 11:18:52 +0300

    feat: added solution for lc 3393

---
+++ contests/lc/3392.CountSubarraysOfLengthThreeWithCondition/count_subarr_length.go
@@ -0,0 +1,10 @@
+package _392_CountSubarraysOfLengthThreeWithCondition
+
+func countSubarrays(nums []int) (ans int) {
+	for i := 1; i+1 < len(nums); i++ {
+		if (nums[i-1]+nums[i+1])*2 == nums[i] {
+			ans++
+		}
+	}
+	return
+}
