8537dfc36bb32c985c3eac67ab4033f65fabf7f8
Andrei Gushchin <neogan74@gmail.com>
2025-05-08 09:36:47 +0300

    feat: added solution for lc 82

---
+++ contests/lc/82.RemoveDuplicatesFromSortedListII/remove_duplicates_from_sorted_list.go
@@ -0,0 +1,23 @@
+package _82_remove_duplicates_from_sorted_list_ii
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre, cur := dummy, head
+	for cur != nil {
+		for cur.Next != nil && cur.Next.Val == cur.Val {
+			cur = cur.Next
+		}
+		if pre.Next == cur {
+			pre = cur
+		} else {
+			pre.Next = cur.Next
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
