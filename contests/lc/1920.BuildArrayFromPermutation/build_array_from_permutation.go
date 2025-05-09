ad4408fe1e7772750cbce95d88339d92873e4d2e
Andrei Gushchin <neogan74@gmail.com>
2025-05-09 17:09:39 +0300

    feat: added solution for lc 1920

---
+++ contests/lc/1920.BuildArrayFromPermutation/build_array_from_permutation.go
@@ -0,0 +1,10 @@
+package _1920_build_array_from_permutation
+
+func buildArray(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	for i := 0; i < n; i++ {
+		ans[i] = nums[nums[i]]
+	}
+	return ans
+}
