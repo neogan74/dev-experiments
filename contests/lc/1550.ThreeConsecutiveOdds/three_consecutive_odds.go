77d3042e613042bb24cc821154ef38856ad45094
Andrei Gushchin <neogan74@gmail.com>
2025-05-12 12:36:33 +0300

    feat: added solution for lc 1550

---
+++ contests/lc/1550.ThreeConsecutiveOdds/three_consecutive_odds.go
@@ -0,0 +1,16 @@
+package __1150
+
+func threeConsecutiveOdds(arr []int) bool {
+	cnt := 0
+	for _, x := range arr {
+		if x&1 == 1 {
+			cnt++
+			if cnt == 3 {
+				return true
+			}
+		} else {
+			cnt = 0
+		}
+	}
+	return false
+}
