6266a071766e759440ad79eb39e611ae115f29ae
Gushchin Andrei <neogan74@gmail.com>
2025-05-15 17:44:13 +0500

    test: added for lc 105

---
+++ contests/lc/105.ConstructBinaryTreeFromPreorderAndInorderTraversal/construct_bin_tree_test.go
@@ -0,0 +1,27 @@
+package _105_construct_binary_tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildTree(t *testing.T) {
+	type args struct {
+		preorder []int
+		inorder  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildTree(tt.args.preorder, tt.args.inorder); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
