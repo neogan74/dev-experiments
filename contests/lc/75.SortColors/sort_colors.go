c786425279a61b58a00e568bba531474e6559dd0
Gushchin Andrei <neogan74@gmail.com>
2025-05-17 19:43:37 +0500

    feat: added solution for lc 75

---
+++ contests/lc/75.SortColors/sort_colors.go
@@ -0,0 +1,17 @@
+package _75_sort_colors
+
+func sortColors(nums []int) {
+	i, j, k := -1, len(nums), 0
+	for k < j {
+		if nums[k] == 0 {
+			i++
+			nums[i], nums[k] = nums[k], nums[i]
+			k++
+		} else if nums[k] == 2 {
+			j--
+			nums[j], nums[k] = nums[k], nums[j]
+		} else {
+			k++
+		}
+	}
+}
