b07acc4bc1ddc1fc0cd3016a5c886c70d1b95dcc
Andrei Gushchin <neogan74@gmail.com>
2025-05-21 14:43:28 +0300

    feat: added solution for lc 86

---
+++ contests/lc/86.PartitionList/partition_list.go
@@ -0,0 +1,23 @@
+package _86_partition_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func partition(head *ListNode, x int) *ListNode {
+	l, r := &ListNode{}, &ListNode{}
+	tl, tr := l, r
+	for ; head != nil; head = head.Next {
+		if head.Val < x {
+			tl.Next = head
+			tl = tl.Next
+		} else {
+			tr.Next = head
+			tr = tr.Next
+		}
+	}
+	tr.Next = nil
+	tl.Next = r.Next
+	return l.Next
+}
