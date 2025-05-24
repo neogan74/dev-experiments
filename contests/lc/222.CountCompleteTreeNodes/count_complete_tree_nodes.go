23fdc15c2156507c001fcd79bc45d28a98c0fc4c
Andrei Gushchin <neogan74@gmail.com>
2025-05-24 10:31:49 +0300

    feat: solution for lc 222

---
+++ contests/lc/222.CountCompleteTreeNodes/count_complete_tree_nodes.go
@@ -0,0 +1,14 @@
+package _222_count_complete_tree_nodes
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
