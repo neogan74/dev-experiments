a5ffff35dcfd1b135ffdca0fce7c4e3e9b237357
Andrei Gushchin <neogan74@gmail.com>
2025-06-01 23:10:25 +0300

    test: added for lc 103

---
+++ contests/lc/103.BinaryTreeZigzagLevelOrderTraversal/btr_zizag_level_test.go
@@ -0,0 +1,52 @@
+package _103_binary_tree_zigzag_level_order
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_zigzagLevelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantAns [][]int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val:   9,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val: 20,
+						Left: &TreeNode{
+							Val:   15,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:   7,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+				},
+			},
+			wantAns: [][]int{{3}, {20, 9}, {15, 7}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAns := zigzagLevelOrder(tt.args.root); !reflect.DeepEqual(gotAns, tt.wantAns) {
+				t.Errorf("zigzagLevelOrder() = %v, want %v", gotAns, tt.wantAns)
+			}
+		})
+	}
+}
