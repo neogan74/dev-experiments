d2042aaaa0590a235ef60a627ec5340e7eb3b65e
Andrei Gushchin <neogan74@gmail.com>
2025-06-02 22:40:39 +0300

    test:  for lc 2918

---
+++ contests/lc/2918.MinimumEqualSumOfTwoArraysAfterReplacingZeros/min_eq_sum_of_two_arrays_test.go
@@ -0,0 +1,31 @@
+package _2918_min_eq_sum_of_two_arrays_after_replacing_zeros
+
+import "testing"
+
+func Test_minSum(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				nums1: []int{3, 2, 0, 1, 0},
+				nums2: []int{6, 5, 0},
+			},
+			want: 12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSum(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("minSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
