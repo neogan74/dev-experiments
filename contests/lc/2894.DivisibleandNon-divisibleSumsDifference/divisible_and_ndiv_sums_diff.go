37a5c7856209b146885bf190a4843bc4e4a716e2
Andrei Gushchin <neogan74@gmail.com>
2025-06-07 20:07:05 +0300

    feat: fix issue lc 2894

--- contests/lc/2894.DivisibleandNon-divisibleSumsDifference/divisible_and_ndiv_sums_diff.go
+++ contests/lc/2894.DivisibleandNon-divisibleSumsDifference/divisible_and_ndiv_sums_diff.go
@@ -1,7 +1,7 @@
 package _2894_DivisibleandNon_divisibleSumsDifference
 
 func differenceOfSum(nums []int, m int) (ans int) {
-	for i := 1; i <= n; i++ {
+	for i := 1; i <= m; i++ {
 		if i%m == 0 {
 			ans -= i
 		} else {
