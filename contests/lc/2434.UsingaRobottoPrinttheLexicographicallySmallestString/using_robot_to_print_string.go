abd3a4de8bbb3895a37c44c5e178b43a3227ac01
Andrei Gushchin <neogan74@gmail.com>
2025-06-08 21:01:59 +0300

    feat: add solution for lc 2434

---
+++ contests/lc/2434.UsingaRobottoPrinttheLexicographicallySmallestString/using_robot_to_print_string.go
@@ -0,0 +1,23 @@
+package _2434_using_robot_to_print_the_lex_smalles_string
+
+func robotWithString(s string) string {
+	cnt := make([]int, 26)
+	for _, c := range s {
+		cnt[c-'a']++
+	}
+	mi := byte('a')
+	stk := []byte{}
+	ans := []byte{}
+	for i := range s {
+		cnt[s[i]-'a']--
+		for mi < 'z' && cnt[mi-'a'] == 0 {
+			mi++
+		}
+		stk = append(stk, s[i])
+		for len(stk) > 0 && stk[len(stk)-1] <= mi {
+			ans = append(ans, stk[len(stk)-1])
+			stk = stk[:len(stk)-1]
+		}
+	}
+	return string(ans)
+}
