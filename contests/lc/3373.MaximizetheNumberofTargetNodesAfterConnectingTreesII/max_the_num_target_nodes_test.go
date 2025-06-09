0cef28d6c37f87135512d90bc3b7cb13a4f62433
Andrei Gushchin <neogan74@gmail.com>
2025-06-09 23:29:39 +0300

    test: 3373

--- contests/lc/3373.MaximizetheNumberofTargetNodesAfterConnectingTreesII/max_the_num_target_nodes_test.go
+++ contests/lc/3373.MaximizetheNumberofTargetNodesAfterConnectingTreesII/max_the_num_target_nodes_test.go
@@ -14,7 +14,9 @@ func Test_maxTargetNodes(t *testing.T) {
 		name string
 		args args
 		want []int
-	}{}
+	}{
+		//TODO:
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := maxTargetNodes(tt.args.edges1, tt.args.edges2); !reflect.DeepEqual(got, tt.want) {
