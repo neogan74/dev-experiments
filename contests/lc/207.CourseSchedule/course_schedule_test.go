02312f29776482ef8d3041ccbf0033d434b0b9f5
Andrei Gushchin <neogan74@gmail.com>
2025-06-10 21:46:55 +0300

    test: for lc 207

---
+++ contests/lc/207.CourseSchedule/course_schedule_test.go
@@ -0,0 +1,39 @@
+package _207_course_schedule
+
+import "testing"
+
+func Test_canFinish(t *testing.T) {
+	type args struct {
+		numCourses    int
+		prerequisites [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				numCourses:    2,
+				prerequisites: [][]int{{1, 0}},
+			},
+			want: true,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				numCourses:    2,
+				prerequisites: [][]int{{1, 0}, {0, 1}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canFinish(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+				t.Errorf("canFinish() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
