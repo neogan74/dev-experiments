cd89d04a3fa7c909fca6328117bf416996215c1b
Andrei Gushchin <neogan74@gmail.com>
2025-07-01 10:49:10 +0300

    test: for lc 162

---
+++ contests/lc/162.FindPeakElement/find-peak-element_test.go
@@ -0,0 +1,36 @@
+package findpeakelement
+
+import "testing"
+
+func Test_findPeakElement(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				nums: []int{1, 2, 3, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "Test 2",
+			args: args{
+				nums: []int{1, 2, 1, 3, 5, 6, 4},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findPeakElement(tt.args.nums); got != tt.want {
+				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
