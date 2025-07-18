8946b516f5128a8f71d940d04674c7fdaf17f399
Andrei Gushchin <neogan74@gmail.com>
2025-07-18 16:43:06 +0300

    test: for lc 1071

---
+++ contests/lc/1071.GreatestCommonDivisorofStrings/greatest-common-divisor-of-strings_test.go
@@ -0,0 +1,31 @@
+package greatestcommondivisorofstrings
+
+import "testing"
+
+func Test_gcdOfStrings(t *testing.T) {
+	type args struct {
+		str1 string
+		str2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test_case_1",
+			args: args{
+				str1: "ABCABC",
+				str2: "ABC",
+			},
+			want: "ABC",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := gcdOfStrings(tt.args.str1, tt.args.str2); got != tt.want {
+				t.Errorf("gcdOfStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
