8ea79f4a2a601087ec5a2835ce4840c5a40aed68
Andrei Gushchin <neogan74@gmail.com>
2025-08-01 13:13:05 +0300

    feat: added solution for lc 118

---
+++ contests/lc/118.PascalsTriangle/pascals-triangle.go
@@ -0,0 +1,13 @@
+package pascalstriangle
+
+func generate(numRows int) [][]int {
+	ans := make([][]int, numRows)
+	for i := range ans {
+		ans[i] = make([]int, i+1)
+		ans[i][0], ans[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+	}
+	return ans
+}
