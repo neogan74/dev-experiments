d199acdc83e37035c13beaeb7072035793a2ac95
Gushchin Andrei <neogan74@gmail.com>
2025-09-06 21:11:23 +0500

    test: lines case for wc

--- Play/wc/wc_test.go
+++ Play/wc/wc_test.go
@@ -9,8 +9,18 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("Word1 word2 word3 word4\n")
 	want := 4
 
-	got := count(b)
+	got := count(b, false)
 	if got != want {
 		t.Errorf("Expected %d, got %d instead.\n", want, got)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2\n line2 word3\n line3 word4\n")
+	want := 3
+
+	got := count(b, true)
+	if want != got {
+		t.Errorf("Expected %d, but got %d lines instead.", want, got)
 	}
 }
