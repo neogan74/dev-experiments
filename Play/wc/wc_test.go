0e1b9f33a16809ddd8f159e4430ed31835fbf08b
Gushchin Andrei <neogan74@gmail.com>
2025-09-06 21:01:55 +0500

    test: one dummy test for wc

---
+++ Play/wc/wc_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountWords(t *testing.T) {
+	b := bytes.NewBufferString("Word1 word2 word3 word4\n")
+	want := 4
+
+	got := count(b)
+	if got != want {
+		t.Errorf("Expected %d, got %d instead.\n", want, got)
+	}
+}
