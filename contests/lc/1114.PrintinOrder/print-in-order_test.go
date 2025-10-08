d030f391969ad4ce0c78303eba89384ac89ee7d8
Gushchin Andrei <neogan74@gmail.com>
2025-10-08 14:37:11 +0500

    test: added bench go solution for lc 1114

--- contests/lc/1114.PrintinOrder/print-in-order_test.go
+++ contests/lc/1114.PrintinOrder/print-in-order_test.go
@@ -49,3 +49,47 @@ func TestFooPrintOrder(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkFoo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		foo := Constructor()
+
+		var buf bytes.Buffer
+		var mu sync.Mutex
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		go func() {
+			defer wg.Done()
+			foo.Third(func() {
+				mu.Lock()
+				buf.WriteString("third")
+				mu.Unlock()
+			})
+		}()
+
+		go func() {
+			defer wg.Done()
+			foo.First(func() {
+				mu.Lock()
+				buf.WriteString("first")
+				mu.Unlock()
+			})
+		}()
+
+		go func() {
+			defer wg.Done()
+			foo.Second(func() {
+				mu.Lock()
+				buf.WriteString("second")
+				mu.Unlock()
+			})
+		}()
+
+		wg.Wait()
+
+		if got, want := buf.String(), "firstsecondthird"; got != want {
+			b.Fatalf("got %q, want %q", got, want)
+		}
+	}
+}
