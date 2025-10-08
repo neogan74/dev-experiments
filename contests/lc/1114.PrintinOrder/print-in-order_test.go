07956a06520675d852bd157126baf4ec591420f9
Gushchin Andrei <neogan74@gmail.com>
2025-10-08 14:35:40 +0500

    test: added go solution for lc 1114

---
+++ contests/lc/1114.PrintinOrder/print-in-order_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+)
+
+func TestFooPrintOrder(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		foo := Constructor()
+
+		var buf bytes.Buffer
+		var mu sync.Mutex
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		go func() {
+			defer wg.Done()
+			foo.Third(func() {
+				mu.Lock()
+				buf.WriteString("third")
+				mu.Unlock()
+			})
+		}()
+
+		go func() {
+			defer wg.Done()
+			foo.First(func() {
+				mu.Lock()
+				buf.WriteString("first")
+				mu.Unlock()
+			})
+		}()
+
+		go func() {
+			defer wg.Done()
+			foo.Second(func() {
+				mu.Lock()
+				buf.WriteString("second")
+				mu.Unlock()
+			})
+		}()
+
+		wg.Wait()
+
+		if got, want := buf.String(), "firstsecondthird"; got != want {
+			t.Fatalf("iteration %d: got %q, want %q", i, got, want)
+		}
+	}
+}
