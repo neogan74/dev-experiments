e4ef2b355c12b6673e193af0809750652a1e5b81
Gushchin Andrei <neogan74@gmail.com>
2025-10-11 08:58:20 +0500

    test: added go solution for lc 1115

---
+++ contests/lc/1115.PrintFooBarAlternately/print-foobar-alternately_test.go
@@ -0,0 +1,32 @@
+package _115_PrintFooBarAlternately
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewFooBar(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *FooBar
+	}{
+		{
+			name: "Test 1",
+			args: args{
+				n: 1,
+			},
+			want: FooBar{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewFooBar(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFooBar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
