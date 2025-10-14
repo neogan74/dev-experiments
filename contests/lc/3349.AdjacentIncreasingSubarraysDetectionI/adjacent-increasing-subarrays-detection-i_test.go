3aa1aef03892cfd124def2b24ecb86c6d33ef1a2
Gushchin Andrei <neogan74@gmail.com>
2025-10-14 22:57:54 +0500

    test: added go solution for lc 3349

--- contests/lc/3349.AdjacentIncreasingSubarraysDetectionI/adjacent-increasing-subarrays-detection-i_test.go
+++ contests/lc/3349.AdjacentIncreasingSubarraysDetectionI/adjacent-increasing-subarrays-detection-i_test.go
@@ -12,7 +12,22 @@ func Test_hasUniqueElements(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test 1",
+			args: args{
+				nums: []int{2, 5, 7, 8, 9, 2, 3, 4, 3, 1},
+				k:    3,
+			},
+			want: true,
+		},
+		{
+			name: "test 2",
+			args: args{
+				nums: []int{1, 2, 3, 4, 4, 4, 4, 5, 6, 7},
+				k:    5,
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
