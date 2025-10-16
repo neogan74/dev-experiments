b9d458b4fbfbb8ff50f4700ae13e811c16530261
Andrei Gushchin <neogan74@gmail.com>
2025-10-16 10:00:07 +0300

    feat: добавлена синхронизация горутин с использованием WaitGroup и мьютекса
    Добавлено корректное ожидание завершения горутин через sync.WaitGroup, исправлено отсутствие синхронизации в примерах. Добавлены реализации решения проблемы гонок с помощью atomic, мьютекса и вывод результата после завершения всех горутин. Добавлен вывод "Finish" по завершении.

--- Play/concurrency/goroutine/endless/main.go
+++ Play/concurrency/goroutine/endless/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+
 	fmt.Println("Just problem case always retunr 0")
-	problem()
+	go problem()
+	wg.Add(1)
 	fmt.Println("Solution of the problem with atomic sync.")
-	solution()
+	go solution()
+	wg.Add(1)
+	fmt.Println("Solution of the problem with mutex")
+	go SolutionMutex()
+	wg.Add(1)
+
+	wg.Wait()
+	fmt.Println("Finish")
 }
 
 // problem that there are no sync between main func and gorotune
@@ -27,6 +39,7 @@ func problem() {
 	}()
 
 	fmt.Println(i)
+	wg.Done()
 }
 
 func solution() {
@@ -41,4 +54,27 @@ func solution() {
 
 	time.Sleep(2 * time.Millisecond)
 	fmt.Println(atomic.LoadInt64(&i))
+	wg.Done()
+}
+
+func SolutionMutex() {
+	runtime.GOMAXPROCS(1)
+
+	var i int
+	var mu sync.Mutex
+
+	go func() {
+		for {
+			mu.Lock()
+			i++
+			mu.Unlock()
+		}
+	}()
+
+	time.Sleep(time.Millisecond)
+
+	mu.Lock()
+	fmt.Println(i)
+	mu.Unlock()
+	wg.Done()
 }
