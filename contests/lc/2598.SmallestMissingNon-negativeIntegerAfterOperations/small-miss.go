486603a7f6714a878282d3ce785a7671b5cd13a6
Gushchin Andrei <neogan74@gmail.com>
2025-10-16 21:22:57 +0500

    feat: added go solution for lc 2598

---
+++ contests/lc/2598.SmallestMissingNon-negativeIntegerAfterOperations/small-miss.go
@@ -0,0 +1,14 @@
+package _598_SmallestMissingNon_negativeIntegerAfterOperations
+
+func findSmallestInteger(nums []int, value int) int {
+	cnt := make([]int, value)
+	for _, x := range nums {
+		cnt[(x%value+value)%value]++
+	}
+	for i := 0; ; i++ {
+		if cnt[i%value] == 0 {
+			return i
+		}
+		cnt[i%value]--
+	}
+}
