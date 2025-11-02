dea9f4b4ee7b2a67175d6ef61783a18eec63908a
Gushchin Andrei <neogan74@gmail.com>
2025-11-02 09:04:17 +0500

    feat(lc2257): update code

--- contests/lc/2257.CountUnguardedCellsintheGrid/solution.py
+++ contests/lc/2257.CountUnguardedCellsintheGrid/solution.py
@@ -1,4 +1,3 @@
-from itertools import pairwise
 from typing import List
 
 
@@ -9,9 +8,9 @@ class Solution:
             g[i][j] = 2
         for i, j in walls:
             g[i][j] = 2
-        dirs = (-1, 0, 1, 0, -1)
+        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
         for i, j in guards:
-            for a, b in pairwise(dirs):
+            for a, b in directions:
                 x, y = i, j
                 while 0 <= x + a < m and 0 <= y + b < n and g[x + a][y + b] < 2:
                     x, y = x + a, y + b
