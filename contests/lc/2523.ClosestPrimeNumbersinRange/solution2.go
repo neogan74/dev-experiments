e239d1a98d5f62d02eb8b0fcf82e432b189c293f
Gushchin Andrei <neogan74@gmail.com>
2025-12-03 11:57:15 +0500

    feat(lc2523): added go solution

---
+++ contests/lc/2523.ClosestPrimeNumbersinRange/solution2.go
@@ -0,0 +1,34 @@
+//go:build ignore
+// +build ignore
+
+package closestprimenumbersinrange
+
+import "math"
+
+func closestPrimes2(left int, right int) []int {
+	isNotPrime := make([]bool, right+1)
+	isNotPrime[0] = true
+	isNotPrime[1] = true
+	upperBound := int(math.Sqrt(float64(right)))
+	for num := 2; num <= upperBound; num++ {
+		if !isNotPrime[num] {
+			for j := num * num; j <= right; j += num {
+				isNotPrime[j] = true
+			}
+		}
+	}
+
+	prev := -1
+	curMin := right
+	ans := []int{-1, -1}
+	for i := left; i < right+1; i++ {
+		if !isNotPrime[i] {
+			if prev != -1 && curMin > i-prev {
+				curMin = i - prev
+				ans = []int{prev, i}
+			}
+			prev = i
+		}
+	}
+	return ans
+}
