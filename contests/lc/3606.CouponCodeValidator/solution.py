b5fc972f9fb5397a3527aec881b5dd59f0decc99
Gushchin Andrei <neogan74@gmail.com>
2025-12-13 10:36:44 +0500

    feat(lc3606): added py solution

---
+++ contests/lc/3606.CouponCodeValidator/solution.py
@@ -0,0 +1,28 @@
+import re
+from typing import List
+
+
+class Solution:
+    def validateCoupons(
+        self, code: List[str], businessLine: List[str], isActive: List[bool]
+    ) -> List[str]:
+        order = {
+            "electronics": 0,
+            "grocery": 1,
+            "pharmacy": 2,
+            "restaurant": 3,
+        }
+        valid_code = re.compile(r"^[A-Za-z0-9_]+$")
+
+        coupons = []
+        for c, b, active in zip(code, businessLine, isActive):
+            if not active:
+                continue
+            if b not in order:
+                continue
+            if not c or not valid_code.fullmatch(c):
+                continue
+            coupons.append((order[b], c))
+
+        coupons.sort(key=lambda x: (x[0], x[1]))
+        return [c for _, c in coupons]
